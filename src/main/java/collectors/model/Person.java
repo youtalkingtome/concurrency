5b188f272ae9fd6cfce5c7910cf0c94308350658
Vikas Taank <vikastaank@vikass-mbp-3.myfiosgateway.com>
2024-10-09 21:34:22 -0400

    Commiting Most recent

--- src/main/java/collectors/model/Person.java
+++ src/main/java/collectors/model/Person.java
@@ -5,11 +5,30 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.*;
 import java.util.Optional;
 
 @Getter
 @Setter@AllArgsConstructor
 public class Person {
+
+    static String readLine(String path) throws IOException {
+        try(BufferedReader br = new BufferedReader(new FileReader(path))){
+            return br.readLine();
+        }
+    }
+
+    static void copy(String src, String dest) throws IOException {
+        try (InputStream in = new FileInputStream(src);
+            OutputStream out = new FileOutputStream(dest)) {
+                byte[] buff = new byte[100000];
+                int n;
+                while ((n = in.read(buff)) >= 0)
+                    out.write(buff, 0, n);
+            }
+        }
+
+
     private Optional<Car> car;
     public Optional<Car> getCar() { return car; }
 }
